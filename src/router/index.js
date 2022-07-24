154428c1960df94968130329ad7dfff60f9c644e
dlf <136975643@qq.com>
2022-07-24 17:26:11 +0800

    my-json-server

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import EventList from "../views/EventList.vue"
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "EventList",
+    component: EventList
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ]
 
---
+++ src/views/EventList.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>善事活动</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ 代表 /src
+import EventCard from '@/components/EventCard.vue'
+import axios from "axios"
+
+export default {
+  name: 'HomeView',
+  // 注册局部组件EventCard
+  components: {
+    EventCard
+  },
+  data () {
+    return {
+      events: null
+    }
+  },
+  created () {
+    axios.get("https://my-json-server.typicode.com/git136975643/vuecli-sfc/events").then(res => {
+      console.log('events', res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
